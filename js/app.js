e774576c2bcb2e9af0a337bb04024e1448d1c094
Ryan Siu <rsiu@stuy.edu>
2017-12-22 22:58:19 -0500

    Add more bios

--- js/app.js
+++ js/app.js
@@ -1,22 +1,44 @@
 $(document).foundation()
 
 var tagmap = function() {
-  $('#ryan').foundation('open');
+  $('#' + this.getAttribute("alt")).foundation('open');
 }
 
 var map = document.getElementById("tagmap");
 var img = document.getElementById("team_pic");
 
-console.log(img.height);
-console.log(img.width);
-
 var scaleX = img.width / 2048;
 var scaleY = img.height / 1365;
 
-var ryan = document.createElement("area");
-ryan.setAttribute("shape", "circle");
-ryan.setAttribute("coords", Math.round(1111*scaleX) + "," + Math.round(666*scaleY) + "," + Math.round(60*scaleX));
-ryan.setAttribute("alt", "ryan");
-//ryan.addEventListener("mouseover", tagmap);
-ryan.addEventListener("click", tagmap);
-map.appendChild(ryan);
+var people = {
+  'ryan': [1111, 666, 60],
+  'jt': [706, 416, 52],
+  'lili': [326, 314, 48],
+  'jon': [886, 670, 60],
+  'max': [1484, 642, 60]
+}
+
+for (var person in people) {
+  var a = document.createElement("area");
+  a.setAttribute("shape", "circle");
+  a.setAttribute("coords", Math.round(people[person][0]*scaleX) + "," + Math.round(people[person][1]*scaleY) + "," + Math.round(people[person][2]*scaleX));
+  a.setAttribute("alt", person);
+  //a.addEventListener("mouseover", tagmap);
+  a.addEventListener("click", tagmap);
+  map.appendChild(a);
+}
+
+var things = {
+  'robot': [774, 1018, 1092, 1286],
+  'motivate': [958, 586, 1000, 720]
+}
+
+for (var thing in things) {
+  var a = document.createElement("area");
+  a.setAttribute("shape", "rect");
+  a.setAttribute("coords", Math.round(things[thing][0]*scaleX) + "," + Math.round(things[thing][1]*scaleY) + "," + Math.round(things[thing][2]*scaleX) + "," + Math.round(things[thing][3]*scaleY));
+  a.setAttribute("alt", thing);
+  //a.addEventListener("mouseover", tagmap);
+  a.addEventListener("click", tagmap);
+  map.appendChild(a);
+}
